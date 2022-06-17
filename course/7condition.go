251e128455c252ab900ba866efa26d40ac4514b5
Dimas Wahyu Notonagoro <dimaswntech@gmail.com>
2022-06-17 14:08:11 +0700

    condition

---
+++ course/7condition.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	var nilai int = 10
+
+	if nilai == 10 {
+		fmt.Println("10")
+	} else if nilai <= 7 {
+		fmt.Println("10<=7")
+	} else {
+		fmt.Println("10>=7")
+	}
+}
