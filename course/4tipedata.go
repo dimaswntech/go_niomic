da38d7f1e185a4c726eed08a20e80245d0829e87
Dimas Wahyu Notonagoro <dimaswntech@gmail.com>
2022-06-17 13:57:31 +0700

    tipe data

---
+++ course/4tipedata.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	var numberPositif int = 10
+	var numberNegatif int = -10
+	var desimal = 2.3
+	var isTrue bool = false
+	var message string = "message"
+
+	fmt.Println(numberPositif)
+	fmt.Println(numberNegatif)
+	fmt.Println(isTrue)
+	fmt.Println(desimal)
+	fmt.Println(message)
+}
