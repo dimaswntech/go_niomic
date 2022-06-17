a9f51cf62ddbdbcc235252e11ca3a4ef93876039
Dimas Wahyu Notonagoro <dimaswntech@gmail.com>
2022-06-17 13:50:28 +0700

    variabel

---
+++ course/3variabel.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	var namadepan string = "Dimas"
+	var namabelakang = "N"
+	namatengah := "W"
+
+	fmt.Printf("Halo %s %s %s", namadepan, namatengah, namabelakang)
+}
