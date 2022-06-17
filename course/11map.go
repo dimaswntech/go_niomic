077cea122e4cf57bc180aae46316dfc70a466029
Dimas Wahyu Notonagoro <dimaswntech@gmail.com>
2022-06-17 22:59:21 +0700

    mappt2

--- course/11map.go
+++ course/11map.go
@@ -4,5 +4,8 @@ import "fmt"
 
 func main() {
 	var jumlahBuah = map[string]int{"apel": 1, "melon": 2}
-	fmt.Println(jumlahBuah["apel"])
+	// fmt.Println(jumlahBuah["apel"])
+	for key, element := range jumlahBuah {
+		fmt.Println(key, element)
+	}
 }
